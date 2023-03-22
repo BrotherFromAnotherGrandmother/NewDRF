63f95f1f60fc631e2604fbcc7c220155a90bf347
BrotherFromAnotherGrandmother <101392966+BrotherFromAnotherGrandmother@users.noreply.github.com>
2023-03-22 16:05:45 +0300

    Переопределил методы get и post

--- NewDRF/women/views.py
+++ NewDRF/women/views.py
@@ -1,3 +1,4 @@
+from django.forms import model_to_dict
 from rest_framework import generics
 
 # Create your views here.
@@ -9,10 +10,16 @@ from .serializers import WomenSerializer
 
 class WomenAPIView(APIView):
     def get(self, request):
-        return Response({'title': 'Angelina Jolie'})
+        lst = Women.objects.all().values()
+        return Response({'posts': list(lst)})
 
     def post(self, request):
-        return Response({'title': 'kik s nogi'})
+        post_new = Women.objects.create(
+            title = request.data['title'],
+            content = request.data['content'],
+            cat_id = request.data['cat_id']
+        )
+        return Response({'post': model_to_dict(post_new)})
 
 # class WomenAPIView(generics.ListAPIView):
 #     queryset = Women.objects.all()
