e08ed00e2e7b84c86ca3a3fd0628c4e32d43e2e5
BrotherFromAnotherGrandmother <101392966+BrotherFromAnotherGrandmother@users.noreply.github.com>
2023-03-22 16:48:29 +0300

    Создал сериализатор и наследовал его от базового класса Serializer Создал функцию encode Создал класс WomenModel

--- NewDRF/women/serializers.py
+++ NewDRF/women/serializers.py
@@ -2,7 +2,18 @@ from rest_framework import serializers
 from .models import Women
 
 
-class WomenSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Women
-        fields = ('title', 'cat_id')
+class WomenModel:
+    def __init__(self, title, content):
+        self.title = title
+        self.content = content
+
+
+class WomenSerializer(serializers.Serializer):
+    title = serializers.CharField(max_length=255)
+    content = serializers.CharField()
+
+
+def encode():
+    model = WomenModel('Angelina Jolie', 'Content: Angelina Jolie')
+    model_sr = WomenSerializer(model)
+    print(model_sr.data, type(model_sr.data, sep='\n'))
