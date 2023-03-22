c35b8a112a9940a0e2a52861badad32aff8dcdf2
BrotherFromAnotherGrandmother <101392966+BrotherFromAnotherGrandmother@users.noreply.github.com>
2023-03-22 15:20:15 +0300

    Переопределил класс WomenAPIView и унаследовал его от базового APIView

--- NewDRF/women/views.py
+++ NewDRF/women/views.py
@@ -1,9 +1,17 @@
 from rest_framework import generics
 
 # Create your views here.
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
 from .models import Women
 from .serializers import WomenSerializer
 
-class WomenAPIView(generics.ListAPIView):
-    queryset = Women.objects.all()
-    serializer_class = WomenSerializer
+class WomenAPIView(APIView):
+    def get(self, request):
+        return Response({'title': 'Angelina Jolie'})
+
+
+# class WomenAPIView(generics.ListAPIView):
+#     queryset = Women.objects.all()
+#     serializer_class = WomenSerializer
