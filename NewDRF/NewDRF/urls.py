93ab2178c82d72d18645f154ee3a35fc92fcf294
BrotherFromAnotherGrandmother <101392966+BrotherFromAnotherGrandmother@users.noreply.github.com>
2023-03-22 14:55:21 +0300

    Исправил ошибку в файле settings.py в подключении приложения rest_framework Дополнил url

--- NewDRF/NewDRF/urls.py
+++ NewDRF/NewDRF/urls.py
@@ -20,5 +20,5 @@ from women.views import WomenAPIView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/womenlist', WomenAPIView),
+    path('api/v1/womenlist', WomenAPIView.as_view()),
 ]
