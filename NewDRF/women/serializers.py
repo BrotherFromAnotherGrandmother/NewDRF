aa3cc919e65f105ed237ab1b0a3c06dbf6488496
BrotherFromAnotherGrandmother <101392966+BrotherFromAnotherGrandmother@users.noreply.github.com>
2023-03-23 21:17:12 +0300

    Добавил проверку на валидность введённых данных при post-запросе

--- NewDRF/women/serializers.py
+++ NewDRF/women/serializers.py
@@ -3,7 +3,7 @@ from rest_framework import serializers
 class WomenSerializer(serializers.Serializer):
     title = serializers.CharField(max_length=255)
     content = serializers.CharField()
-    time_create = serializers.DateTimeField()
-    time_update = serializers.DateTimeField()
+    time_create = serializers.DateTimeField(read_only=True)
+    time_update = serializers.DateTimeField(read_only=True)
     is_published = serializers.BooleanField(default=True)
     cat_id = serializers.IntegerField()
