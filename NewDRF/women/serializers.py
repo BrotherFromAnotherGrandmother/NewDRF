059b9f46d0f451102f59f7b7e11cc91c536d6629
BrotherFromAnotherGrandmother <101392966+BrotherFromAnotherGrandmother@users.noreply.github.com>
2023-03-23 18:32:08 +0300

    Написал функцию декодирования данных из JSON строки обратно в объект класса WomenModel

--- NewDRF/women/serializers.py
+++ NewDRF/women/serializers.py
@@ -1,4 +1,7 @@
+import io
+
 from rest_framework import serializers
+from rest_framework.parsers import JSONParser
 from rest_framework.renderers import JSONRenderer
 
 from .models import Women
@@ -21,3 +24,10 @@ def encode():
     print(model_sr.data, type(model_sr.data), sep='\n')
     json = JSONRenderer().render(model_sr.data)
     print(json)
+
+def decode():
+    stream = io.BytesIO(b'{"title": "Angelina Jolie", "content": "Content: Angelina Jolie"}')
+    data = JSONParser().parse(stream)
+    serializer = WomenSerializer(data=data)
+    serializer.is_valid()
+    print(serializer.validated_data)
