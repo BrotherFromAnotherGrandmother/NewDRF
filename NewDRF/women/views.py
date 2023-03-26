062b9651d63144fe5801e7a8fee7e3c79ab7a657
BrotherFromAnotherGrandmother <101392966+BrotherFromAnotherGrandmother@users.noreply.github.com>
2023-03-26 19:30:48 +0300

    Изменил наследование класса WomenViewSet на всех родителей класса ModelViewSet

--- NewDRF/women/views.py
+++ NewDRF/women/views.py
@@ -1,13 +1,19 @@
 from django.forms import model_to_dict
-from rest_framework import generics, viewsets
+from rest_framework import generics, viewsets, mixins
 
 # Create your views here.
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.viewsets import GenericViewSet
 
 from .models import Women
 from .serializers import WomenSerializer
 
-class WomenViewSet(viewsets.ReadOnlyModelViewSet):
+class WomenViewSet(mixins.CreateModelMixin,
+                   mixins.RetrieveModelMixin,
+                   mixins.UpdateModelMixin,
+                   mixins.DestroyModelMixin,
+                   mixins.ListModelMixin,
+                   GenericViewSet):
     queryset = Women.objects.all()
     serializer_class = WomenSerializer
