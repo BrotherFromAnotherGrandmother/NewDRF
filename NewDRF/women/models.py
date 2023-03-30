3499f597670939d82b95acc460e5a7ae26dfc414
BrotherFromAnotherGrandmother <101392966+BrotherFromAnotherGrandmother@users.noreply.github.com>
2023-03-30 17:50:06 +0300

    Удалил строчку time_update из модели Women. С ней выдавало ошибку

--- NewDRF/women/models.py
+++ NewDRF/women/models.py
@@ -6,7 +6,6 @@ class Women(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField(blank=True)
     time_create = models.DateTimeField(auto_now_add=True)
-    time_update = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=True)
     cat = models.ForeignKey('Category', on_delete=models.PROTECT, null=True)
     user = models.ForeignKey(User, verbose_name='Пользователь', on_delete=models.CASCADE)
