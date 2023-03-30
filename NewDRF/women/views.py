7e7290c80fd2d9ead858304864eec9a4273add4a
BrotherFromAnotherGrandmother <101392966+BrotherFromAnotherGrandmother@users.noreply.github.com>
2023-03-30 18:33:01 +0300

    Удалил ненужный импорт Редактировал файл согласно PEP8

--- NewDRF/women/views.py
+++ NewDRF/women/views.py
@@ -1,13 +1,5 @@
-from django.forms import model_to_dict
-from rest_framework import generics, viewsets, mixins
-
-# Create your views here.
-from rest_framework.decorators import action
+from rest_framework import generics
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAdminUser
-from rest_framework.response import Response
-from rest_framework.views import APIView
-from rest_framework.viewsets import GenericViewSet
-
 from .models import Women, Category
 from .serializers import WomenSerializer
 
@@ -15,13 +7,15 @@ from .serializers import WomenSerializer
 class WomenAPIList(generics.ListCreateAPIView):
     queryset = Women.objects.all()
     serializer_class = WomenSerializer
-    permission_classes = (IsAuthenticatedOrReadOnly, )
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+
 
 class WomenAPIUpdate(generics.RetrieveUpdateAPIView):
     queryset = Women.objects.all()
     serializer_class = WomenSerializer
 
+
 class WomenAPIDestroy(generics.RetrieveDestroyAPIView):
     queryset = Women.objects.all()
     serializer_class = WomenSerializer
-    permission_classes = (IsAdminUser, )
+    permission_classes = (IsAdminUser,)
