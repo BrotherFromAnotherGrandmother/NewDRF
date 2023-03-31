9e8acce9dda0172f8934fa267dcc2f505991d40c
BrotherFromAnotherGrandmother <101392966+BrotherFromAnotherGrandmother@users.noreply.github.com>
2023-03-31 17:58:21 +0300

    Вернул ограничение доступа к по-умолчанию записанному в rest_framework-е (AllowAny)

--- NewDRF/NewDRF/settings.py
+++ NewDRF/NewDRF/settings.py
@@ -130,6 +130,6 @@ REST_FRAMEWORK = {
         'rest_framework.renderers.BrowsableAPIRenderer',
     ],
     'DEFAULT_PERMISSION_CLASSES':[
-        'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.AllowAny',
     ]
 }
