e6ce9a02b80f3f7bfd79244fcf73358008f2c1d6
BrotherFromAnotherGrandmother <101392966+BrotherFromAnotherGrandmother@users.noreply.github.com>
2023-04-03 10:00:33 +0300

    Изменил DEFAULT_AUTHENTICATION_CLASSES с кортежа на список

--- NewDRF/NewDRF/settings.py
+++ NewDRF/NewDRF/settings.py
@@ -128,9 +128,9 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny',
     ],
-    'DEFAULT_AUTHENTICATION_CLASSES': (
+    'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
-    ),
+    ],
 }
